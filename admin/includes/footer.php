7cdee2b268e36512ec06bf1720c3f58626f770be
orumagideon <orumagideon5352gmail.com>
2024-02-15 17:42:05 +0300

    update

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>&copy; 2024 - Library Management System <a href="https://www.alxafrica.com/">ALX</a></strong>
+</footer>
